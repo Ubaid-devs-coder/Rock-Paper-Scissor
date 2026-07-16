c41989ec515fffe9d43460cfa317ce1c8b40b9d4
Ubaid Ur Rehman <ubaid11357@gmail.com>
2026-07-16 23:46:12 +0500

    Update 12-rock-paper-scissors.js

--- 12-rock-paper-scissors.js
+++ 12-rock-paper-scissors.js
@@ -136,9 +136,9 @@ function playGame(playerMove) {
   document.querySelector('.js-result').innerHTML = result
 
   document.querySelector('.js-moves').innerHTML = `You
-    <img src="images/${playerMove}-emoji.png" class="move-icon">
-    <img src="images/${computerMove}-emoji.png" class="move-icon">
-    computer`
+   <img src="images/${playerMove.toLowerCase()}-emoji.png" class="move-icon">
+   <img src="images/${computerMove.toLowerCase()}-emoji.png" class="move-icon">
+computer`;
 
 }
 
